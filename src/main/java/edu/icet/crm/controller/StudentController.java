b5652e1850305b8589bbf6739da496c44cfa37e9
Dulmika <semaldulmika@gmail.com>
2024-02-07 11:48:27 +0530

    student micro service

--- src/main/java/edu/icet/crm/controller/StudentController.java
+++ src/main/java/edu/icet/crm/controller/StudentController.java
@@ -10,8 +10,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-@RestController
 @CrossOrigin
+@RestController
 @RequestMapping(path = "/student")
 public class StudentController {
     @Autowired
